e09ff397ff52914934cddb0ae2af30a52156cf95
ais <bxbygry@gmail.com>
2024-11-01 17:25:29 +0700

    navigate

---
+++ App/penjual/homescreen_penjual.js
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function ({ navigation }) {
+  return (
+    <View style={styles.container}>
+      <Text>homescreen penjual</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
